23c9b4b131926988baf24adbf633aa624d8bc7d5
Min Tie <2084419980@qq.com>
2025-12-01 03:37:34 +0800

    fix: 优惠券分发

--- distribution/src/main/java/com/nageoffer/onecoupon/distribution/DistributionApplication.java
+++ distribution/src/main/java/com/nageoffer/onecoupon/distribution/DistributionApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class DistributionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DistributionApplication.class, args);
     }
